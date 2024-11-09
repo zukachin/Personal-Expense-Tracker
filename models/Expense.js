77f34f81438abcade30f6e1db4272b3e3fbb6785
zukachin <tswetha3188@gmail.com>
2024-11-09 14:20:23 +0530

    folders added

---
+++ models/Expense.js
@@ -0,0 +1,12 @@
+// The Expense model represents each expense record stored in MongoDB.
+
+const mongoose = require('mongoose')
+
+const expenseSchema = new mongoose.Schema({
+    name:{type:String, required: true},
+    amount:{type:Number, required: true},
+    date:{type:Date, required: true},
+    category:{type:String, required: true},
+})
+
+module.exports = mongoose.model('Expense', expenseSchema)
